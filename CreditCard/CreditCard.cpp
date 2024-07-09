842c33a76fea966dda7515b00dca01bd3386fe42
Mario Wijaya <mwijaya3@hotmail.com>
2024-07-09 01:14:57 -0700

    add cmath for ceil function

--- CreditCard/CreditCard.cpp
+++ CreditCard/CreditCard.cpp
@@ -1,35 +1,42 @@
 
-#include <iostream> 
+#include <iostream>
+#include <cmath>
 #include "CreditCard.h"
 
-using namespace std; 
+using namespace std;
 
-ostream& operator << (ostream& os, const CreditCard &c){
-    os  << "Name: " << c.name
-        << "Balance: " << c.balance
-        << "Limit: " << c.limit; 
-    return os; 
+ostream &operator<<(ostream &os, const CreditCard &c)
+{
+    os << "Name: " << c.name
+       << "Balance: " << c.balance
+       << "Limit: " << c.limit;
+    return os;
 }
-bool CreditCard:: chargeIt(double price){
-    if (price + balance > limit) {
-        cout << "Card declined!" << endl; 
-        return false; 
+bool CreditCard::chargeIt(double price)
+{
+    if (price + balance > limit)
+    {
+        cout << "Card declined!" << endl;
+        return false;
     }
-    balance += price; 
+    balance += price;
 }
 
-void CreditCard::getPayment(double payment){
-    if (balance <= 0) {
-        cout << "Your balance is 0" << endl; 
-        balance = 0; 
-        return; 
+void CreditCard::getPayment(double payment)
+{
+    if (balance <= 0)
+    {
+        cout << "Your balance is 0" << endl;
+        balance = 0;
+        return;
     }
-    balance -= payment; 
+    balance -= payment;
 }
 
-void CreditCard:: overdue(){
-    float APR_penalty = 0.299; 
-    double interest = balance * APR_penalty; 
+void CreditCard::overdue()
+{
+    float APR_penalty = 0.299;
+    double interest = balance * APR_penalty;
     balance += interest;
-    balance = ceil(balance * 100) / 100; 
+    balance = ceil(balance * 100) / 100;
 }
